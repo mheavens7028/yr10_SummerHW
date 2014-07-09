05d611b37e21ce1c9cbf00c29a3bb83cbb8772bd
Matt <mheavens7028@sohamcollege.org.uk>
2014-07-09 11:11:01 +0100

    Update tables.py

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,15 @@
-#paste code here
+#Summer Homework Times Tables#
+
+n = 1
+times = int(input("What times table would you like to print? (1-12) >> "))
+
+if times <= 12:
+    if times >= 1:
+        for x in range(12):
+            print(times*n)
+            n = n+1
+    else:
+        print("Please choose a number in range.")
+
+else:
+    print("Please choose a number in range.")
